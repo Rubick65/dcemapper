c72d1aebf32bcae8c6bb727dc3b67e965fa64e71
Hugo de Pablo <hugdpalo@gmail.com>
2026-03-13 11:23:41 +0100

    Image selector and NiftyCanvas Creation

---
+++ src/ui/FigureCanvas/NiftyCanvas.py
@@ -0,0 +1,110 @@
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib import colormaps
+
+class NiftyCanvas(FigureCanvas):
+    """
+    Class to create a matpplotlib figure in a PyQT6 interface
+    """
+    def __init__(self, np_array):
+        self.fig, self.axes = plt.subplots()
+        self.axes.axis('off') #Remove the axis numbers
+        super().__init__(self.fig)
+
+        self.data = np_array
+        self.cmap = 'gray' #Color map, by default in black and white colors
+        self.current_z = 0 #Current slide
+        self.current_t = 0 #Current time
+
+        self.max_z = self.data.shape[2] - 1 #Max number of slides in np array
+        self.max_t = self.data.shape[3] - 1 #Max number of seconds in np array
+
+        self.current_slice = self.data[:, :,self.current_z,self.current_t]
+
+        #Image of the np array
+        self.img_slice = self.axes.imshow(self.current_slice, cmap = self.cmap)
+
+        #Event click in the matplotlib image
+        self.mpl_connect('button_press_event', self._on_click)
+
+        #parameter to save and external function
+        self.pixel_callback = None
+
+    #To change the current time and reload the image
+    def set_t(self,t_index):
+        if t_index < 0 or t_index > self.max_t:
+            raise IndexError(f"Error: T={t_index} Out of range (0-{self.max_t})")
+        self.current_t = t_index
+        self.load_image()
+
+    #To change the current slice and reload the image
+    def set_z(self,z_index):
+        if z_index < 0 or z_index > self.max_z:
+            raise IndexError(f"Error: Z={z_index} Out of range (0-{self.max_z})")
+        self.current_z = z_index
+        self.load_image()
+
+    #To change the current cmap and reload the image
+    def set_cmap(self, cmap_name):
+        if cmap_name not in colormaps:
+            self.cmap = 'gray'
+        self.cmap = cmap_name
+        self.img_slice.set_cmap(cmap_name)
+
+    #The image with the current changes is loaded.
+    def load_image(self):
+        self.current_slice = self.data[:, :, self.current_z, self.current_t] #We update de slice with the changes
+        self.img_slice.set_data(self.current_slice)
+        self.draw()
+
+    def _on_click(self, event):
+        #If the click are not in the image limits we ignored
+        if event.inaxes != self.axes:
+            return
+
+        #If the click have valid coordinates
+        if event.xdata is not None and event.ydata is not None:
+            x = int(round(event.xdata))
+            y = int(round(event.ydata))
+
+            #Current numbers of the rows and columns
+            rows, columns = self.current_slice.shape[:2]
+
+            #We checked that the coordinate rounding
+            #did not go beyond the limits of the image
+            if 0 <= y < rows and 0 <= x < columns:
+                value = self.current_slice[y, x]
+
+                #If the image have and external function, we execute
+                if self.pixel_callback:
+                    self.pixel_callback(x, y, value)
+
+    #Allows to the main window to hear the clicks
+    def set_pixel_observer(self, callback_func):
+        self.pixel_callback = callback_func
+
+#Prueba para ver solo
+def mostrar_click (x,y,value):
+    print(f"X = {x}, Y = {y}, intensity = {value}")
+
+app = QApplication(sys.argv)
+
+data_prueba = np.random.rand(256, 256, 10, 5)
+
+ventana = QMainWindow()
+canvas = NiftyCanvas(data_prueba)
+canvas.set_pixel_observer(mostrar_click)
+
+layout = QVBoxLayout()
+layout.addWidget(canvas)
+
+widget_central = QWidget()
+widget_central.setLayout(layout)
+ventana.setCentralWidget(widget_central)
+
+ventana.show()
+sys.exit(app.exec())
